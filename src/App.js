415cd6a6174012d80ecdf3e975bb3951237966ec
Carlos Reyes <carmandoreyes93@gmail.com>
2021-06-29 23:27:56 -0500

    life cycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+function App () {
+  // Codigo para ejemplo y explicación
+  const [counter, setCounter] = useState(0)
+  const [minusCounter, setMinusCounter] = useState(0)
+
+  const [data, setData] = useState(null)
+
+  // Como hacer una petición
+  useEffect(() => {
+    const metodo = async () => {
+      let url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+      const response = await fetch(url).then(res => res.json())
+      setData(response)
+    }
+
+    metodo()
+  }, [])
+
+  useEffect(() => {
+    if (data) {
+      console.log(data.base_experience)
+    }
+  }, [data])
+
+  // useEffect(() => {
+  //   let i = 0
+  //   const sub = setInterval(() => {
+  //     console.log(i)
+  //     i++
+  //   }, 1000)
+
+  //   return () => {
+  //     console.log('Sub:', sub)
+  //     clearInterval(sub)
+  //   }
+  // }, [minusCounter])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <header className='App-header'>
+        <h3>{counter}</h3>
+        <button
+          onClick={() => {
+            setCounter(counter + 1)
+            setMinusCounter(minusCounter - 1)
+          }}
         >
-          Learn React
-        </a>
+          Uno mas
+        </button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
